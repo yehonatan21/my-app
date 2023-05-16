4df865a84df2273e53b023cb5ab0330121402d9b
yehonatan <yehonatan349@gmail.com>
2023-05-16 10:09:24 +0300

    and params to .env

--- back/main.py
+++ back/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.params import Depends
@@ -11,20 +12,25 @@ from routers.auth import router as auth_router
 from routers.mail import router as mail_router
 import logging
 
+DB_IP = os.getenv('DB_IP')
+DB_PORT = int(os.getenv('DB_PORT'))
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 handler = logging.StreamHandler()
 handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
 def connect_to_db(db='Reco', host='127.0.0.1', port=27017):
-    logger.info(db)
-    logger.info(host)
-    logger.info(port)
+    # logger.info(db)
+    # logger.info(host)
+    # logger.info(port)
+    # logger.info(db=db, host=host, port=port)
 
     client = connect(db=db, host=host, port=port)
     logger.info('Connected Successfully')
@@ -32,10 +38,13 @@ def connect_to_db(db='Reco', host='127.0.0.1', port=27017):
     return client
 
 
-connect_to_db(db='Reco', host='127.0.0.1', port=27017)
+# logger.info(DB_IP)
+# logger.info(DB_PORT)
+connect_to_db(db='Reco', host=DB_IP, port=DB_PORT)
 
 # BUG: how to fix it to be cross over all origins
 origins = [
+    "http://localhost"
     "http://localhost:8081"
 ]
 
@@ -50,7 +59,8 @@ app.add_middleware(
 )
 app.include_router(users_router, prefix='/user')
 app.include_router(auth_router, prefix='/auth')
-app.include_router(mail_router, dependencies=[Depends(JWTBearer())], prefix='/mail')
+app.include_router(mail_router, dependencies=[
+                   Depends(JWTBearer())], prefix='/mail')
 
 
 @app.get("/hello", tags=["hello"])
--- front/src/api/config.ts
+++ front/src/api/config.ts
@@ -1,8 +1,8 @@
 import env from 'env-var'
 
 export const config = {
-    PORT: env.get("PORT").default(8000).asInt(),
-    IP: env.get("IP").default("127.0.0.1").asString(),
+    FRONT_PORT: env.get("FRONT_PORT").default(8000).asInt(),
+    FRONT_IP: env.get("FRONT_IP").default("127.0.0.1").asString(),
     DB_NAME: env.get("DB_NAME").default("Reco").asString(),
     DB_URL: env.get("DB_IP").default("mongodb://127.0.0.1:27017").asString(),
   };
